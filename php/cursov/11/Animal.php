95e2288072ec355a535b88cb26cf633c3658c639
Vitor Cunha <104070767+Vitorcalves@users.noreply.github.com>
2023-08-15 20:34:10 -0300

    classe mae animal

--- php/cursov/11/Animal.php
+++ php/cursov/11/Animal.php
@@ -2,16 +2,33 @@
     abstract class Animal{
         protected $peso;
         protected $idade;
-        protected $menbros;
+        protected $membros;
 
         abstract function locomover();
         abstract function alimentar();
         abstract function emitirSom();
 
         public function getPeso(){
-            echo"";
+            return $this->peso;
         
         }
+        public function setPeso($p)
+        {
+            $this->peso = $p;
+        }
         
+        public function getIdade(){
+            return $this->idade;
+        }
+        public function setIdade($i){
+            $this->idade = $i;
+        }
+
+        public function getMembros(){
+            return $this->membros;
+        }
+        public function setMenbros($m){
+            $this->membros = $m;
+        }
     }
 ?>
