6c9a3854c937d6e820131158728ac138ce1b316e
vitor <104070767+Vitorcalves@users.noreply.github.com>
2023-11-08 20:18:38 -0300

    criar nova comanda

---
+++ ComandasIntegrador/insertComanda.php
@@ -0,0 +1,32 @@
+<?php
+    require_once "library/Database.php";
+
+    if (isset($_POST['MESA_ID_MESA'])) {
+
+        $db = new Database();
+        $criacao = date('Y-m-d H:i:s');
+
+        try {
+
+            $result = $db->dbInsert("INSERT INTO comanda
+                                    (DESCRICAO_COMANDA, MESA_ID_MESA, DATA_ABERTURA)
+                                    VALUES (?, ?, ?)",
+                                    [
+                                        $_POST['DESCRICAO_COMANDA'],
+                                        $_POST['MESA_ID_MESA'],
+                                        $criacao
+                                    ]);
+
+            if ($result) {
+                return header("Location: index.php?msgSucesso=Registro inserido com sucesso.");
+            } else {
+                return header("Location: index.php?msgError=Falha ao tentar inserir o registro.");
+            }
+            
+        } catch (Exception $ex) {
+            echo '<p style="color: red;">ERROR: '. $ex->getMessage(). "</p>";
+        }
+
+    } else {
+        return header("Location: index.php");
+    }
